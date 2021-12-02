c170b2b641f26e85bcd5369c05ab88b73e2568c2
Viviola <viviolaputri@gmail.com>
2021-12-02 18:07:13 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/habitapp/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.dicoding.habitapp.utils
+
+const val HABIT = "HABIT"
+const val HABIT_ID = "HABIT_ID"
+const val HABIT_TITLE = "HABIT_TITLE"
+const val NOTIFICATION_CHANNEL_ID = "notify-channel"
+const val NOTIFICATION_CHANNEL_ID_NUMBER = 101
+
+// added by myself
+const val HABIT_TABLE_NAME = "habits"
+
